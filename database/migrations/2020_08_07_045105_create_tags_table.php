481a2c16bb4eea6107dcd00d47ca346cdd4ca961
giangvincent <giang.dlinh92@gmail.com>
2020-08-10 06:44:26 +0700

    add tag to database

---
+++ database/migrations/2020_08_07_045105_create_tags_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('status')->default('punlish');
+            $table->timestamps();
+        });
+        Schema::create('content_tag', function (Blueprint $table) {
+            $table->unsignedBigInteger('content_id');
+            $table->unsignedBigInteger('tag_id');
+
+            $table->primary(['content_id', 'tag_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
